1c7883c7ec4ac278210bc7be58081eaaeeb05d2d
Edison <edisonxu@outlook.com>
2018-08-08 10:09:14 +0800

    create springboot project

---
+++ stock-analysis-springboot/src/main/java/com/ex/demo/sa/StockAnalysisSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.ex.demo.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalysisSpringbootApplication.class, args);
+	}
+}
